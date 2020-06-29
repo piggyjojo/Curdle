a1ee7f80599059df3ec79bf560c75b79d64e7dde
galle <aahmed.s@vivogame11.net>
2020-06-29 15:38:09 -0700

    Added a bunch of Abfel Struddels

--- app/src/main/java/com/example/currtal/MainActivity.kt
+++ app/src/main/java/com/example/currtal/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.example.currtal
 
-import android.opengl.Visibility
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ActionProvider
-import kotlinx.android.synthetic.main.activity_main.*
 import android.view.View
-import android.widget.Switch
-import kotlinx.android.synthetic.main.activity_main.view.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        featureTest.visibility = View.INVISIBLE
+        currency1.visibility = View.INVISIBLE
+        currency2.visibility = View.INVISIBLE
+        currency3.visibility = View.INVISIBLE
+        edit1.visibility = View.INVISIBLE
+        edit2.visibility = View.INVISIBLE
+        edit3.visibility = View.INVISIBLE
+
 
         var selectOpen = false
         setUp.visibility = View.INVISIBLE
         val tester = arrayListOf<String>()
         var currencyMade = 0
         val newCurrency = setUp.text
+
 
 
         select.setOnClickListener {
@@ -35,29 +38,40 @@ class MainActivity : AppCompatActivity() {
 
         setUp.setOnClickListener {
             tester.add(newCurrency.toString())
-            currency1.text = tester.toString()
+            when (View.INVISIBLE) {
+                currency1.visibility -> {
+                    currency1.text = tester[0]
+                    currency1.visibility = View.VISIBLE
+                    edit1.visibility = View.VISIBLE
+                }
+                currency2.visibility -> {
+                    currency2.text = tester[1]
+                    currency2.visibility = View.VISIBLE
+                    edit2.visibility = View.VISIBLE
+                }
+                currency3.visibility -> {
+                    currency3.text = tester[2]
+                    currency3.visibility = View.VISIBLE
+                    edit3.visibility = View.VISIBLE
+                }
+            }
             setUp.text.clear()
             setUp.visibility = View.INVISIBLE
-            if (selectOpen)
-                selectOpen = !selectOpen
+            selectOpen = !selectOpen
         }
 
-        if (currency1.visibility == View.VISIBLE) {
-            featureTest.visibility = View.VISIBLE
 
-            edit1.setOnClickListener {
-                setUp.visibility = View.VISIBLE
-                currency1.text = tester.toString()
-                tester[0] =
-                    (newCurrency.toString())
+        edit1.setOnClickListener {
+            setUp.visibility = View.VISIBLE
+            currency1.text = tester.toString()
+            tester[0] = (newCurrency.toString())
 
-                if (selectOpen)
-                    selectOpen = !selectOpen
-                setUp.text.clear()
-            }
-
+            if (selectOpen)
+                selectOpen = !selectOpen
+            setUp.text.clear()
         }
 
     }
+
 }
 
